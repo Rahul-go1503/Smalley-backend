98f7136f31d951931e749fcf21e04c0c4c08739c
Rahul Goyal <rahulgoyal01052003@gmail.com>
2023-05-15 19:04:49 +0530

    change database

--- app.js
+++ app.js
@@ -17,17 +17,17 @@ app.use(cors());
 //   next();
 // }
 // app.use(myMiddleware);
+app.use("/books", bookRoute); // localhost:5000/books
+// app.use("/blogs", blogRoute); // localhost:5000/books
 app.use('/',(req,res)=>{
   res.send('hello!! Im here...')
 
 })
-app.use("/books", bookRoute); // localhost:5000/books
-// app.use("/blogs", blogRoute); // localhost:5000/books
 // frkHFNZZDONtwOOD
 mongoose
   .connect(process.env.DB_URI)
   .then(() => console.log("Connected To Database")) 
   .then(() => {
-    app.listen(process.env.port || 5000);
+    app.listen(process.env.PORT || 5000);
   })
   .catch((err) => console.log(err));
